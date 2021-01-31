877152de73352f74cc7c285ed16ee23f7341a3c4
trudova <liubovtrudova@gmail.com>
2021-01-31 10:59:44 -0500

    WebDriverFactory set up class

--- src/test/java/com/trycloud/utilities/WebDriverFactory.java
+++ src/test/java/com/trycloud/utilities/WebDriverFactory.java
@@ -1,5 +1,21 @@
 package com.trycloud.utilities;
 
-public class WebDriverFactory {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+public class WebDriverFactory {
+public static WebDriver getDriver(String webBrowserTyp){
+    if(webBrowserTyp.equalsIgnoreCase("chrome")){
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }else if(webBrowserTyp.equalsIgnoreCase("firefox")){
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver();
+    }else {
+        System.err.println("only chrome or fireFox is aloud!");
+        return null;
+    }
+}
 }
