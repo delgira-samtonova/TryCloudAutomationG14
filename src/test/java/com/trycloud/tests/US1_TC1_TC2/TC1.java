e0f29b2c8fb23af46cb3ed8f3daf775dd00fd52e
trudova <liubovtrudova@gmail.com>
2021-02-01 19:58:39 -0500

    test

--- src/test/java/com/trycloud/tests/US1_TC1_TC2/TC1.java
+++ src/test/java/com/trycloud/tests/US1_TC1_TC2/TC1.java
@@ -7,23 +7,12 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TC1 extends TestBase {
-    @BeforeMethod
-    @Override
-    public void setUp() {
-        super.setUp();
-    }
 
     @Test
     public void logInVerification() {
         String expectedURL = "http://qa.trycloud.net/index.php/apps/files/";
         String actualURL = driver.getCurrentUrl();
         Assert.assertEquals(actualURL,expectedURL);
-
     }
 
-    @AfterMethod
-    @Override
-    public void tearDown() {
-        super.tearDown();
-    }
 }
