0c62ecfb21b17528de3a9c82ced8649f8bc6f1cd
Paul Morgan <psamoylov@gmail.com>
2021-02-08 19:02:14 -0500

    US04 test cases 01 and 02 are done

--- src/test/java/com/trycloud/tests/base/TestBase.java
+++ src/test/java/com/trycloud/tests/base/TestBase.java
@@ -6,7 +6,9 @@ import com.trycloud.utilities.Driver;
 import com.trycloud.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import java.util.ArrayList;
