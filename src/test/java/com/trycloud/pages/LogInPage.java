269ebccf01e0ddb4db2bad6d0390e8ba6706e330
trudova <liubovtrudova@gmail.com>
2021-02-09 21:14:30 -0500

    LogInPage

--- src/test/java/com/trycloud/pages/LogInPage.java
+++ src/test/java/com/trycloud/pages/LogInPage.java
@@ -1,4 +1,22 @@
 package com.trycloud.pages;
 
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LogInPage {
+    public LogInPage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(id = "user")
+    public WebElement loginInput;
+
+    @FindBy(id = "password")
+    public WebElement passwordInput;
+
+    @FindBy(id = "submit-form")
+    public WebElement logInButton;
+    @FindBy(xpath = "//p[@class='warning wrongPasswordMsg']")
+    public WebElement warningMessage;
 }
