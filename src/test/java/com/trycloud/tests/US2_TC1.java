abaee400ce51a7fb47c2fba42d71eb0cda5663f8
delgira-samtonova <delgira-samtonova@users.noreply.github.com>
2021-02-11 04:24:56 +0300

    US3_TC1 commit 1

---
+++ src/test/java/com/trycloud/tests/US2_TC1.java
@@ -0,0 +1,32 @@
+package com.trycloud.tests;
+
+import com.trycloud.tests.base.TestBase;
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class US2_TC1 extends TestBase {
+
+    @Test
+    public void fileTitleVerification(){
+
+        /*
+        US2: As a user, I should be able to access all the main modules of the app.
+        Test case #1 - verify users access to the main modules
+        1. Login as a user
+        2. Verify the user see the following modules:
+        Files Galleries Activity Talk Contacts Calendar Note
+        ( Expected module name: Galleries
+        Actual module name : Photos )
+         */
+
+
+
+
+
+
+
+    }
+}
---
+++ src/test/java/com/trycloud/tests/US3_TC1.java
@@ -0,0 +1,30 @@
+package com.trycloud.tests;
+
+import com.trycloud.tests.base.TestBase;
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class US3_TC1 extends TestBase {
+
+    /*
+    US3: As a user, I should be able to access to Files module.
+    Test case #1 - verify users can access to Files module
+    1. Login as a user
+    2. Verify the page title is Files module’s title
+     */
+
+    @Test
+    public void verifyFilesTitle(){
+
+        Driver.getDriver().findElement(By.xpath("//a[@aria-label='Files']")).click();
+
+        String actualFilesTitle = Driver.getDriver().getTitle();
+        String expectedFilesTitle ="Files - Trycloud";
+        Assert.assertTrue(actualFilesTitle.equals(expectedFilesTitle));
+
+        Driver.closeDriver();
+
+    }
+}
